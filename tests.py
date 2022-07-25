dda41bcb851cf00977f4ae168f477387b2fc65ae
Tomáš Mikula <mail@tomasmikula.cz>
2022-07-25 10:14:13 +0200

    Update

--- tests.py
+++ tests.py
@@ -8,8 +8,8 @@ from passwords import password_generator
     "length, count",
     [(5, 60466176), (4, 1679616)],
 )
-def test_rows_even_numbers(length, count):
-    iterator = password_generator(length)
+def test_password_generator(length, count):
+    iterator = password_generator(length=length)
     assert isinstance(iterator, Iterator)
     assert len(list(iterator)) == count
 
